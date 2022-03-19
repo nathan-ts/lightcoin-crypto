ce823172328d293af03ff852b06bca0567f0b860
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-19 03:45:51 +0000

    updated logic

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+  get balance() {
+    if (this.transactions.length === 0) return 0;
+    return this.transactions.reduce((a, b) => a + b.value, 0);
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
   commit() {
-    balance -= this.amount;
+    console.log(`the value is ${this.value} and the balance is ${this.account.balance}`);
+    if (this.account.balance + this.value < 0) {
+      console.log(`You cannot perform this transaction: insufficient balance (${this.account.balance}).`);
+    } else {
+      this.time = new Date(); // Keep track of the time of the transaction
+      this.account.addTransaction(this); // Add the transaction to the account
+    }
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
   }
 
 }
@@ -17,13 +53,20 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("chasing-cars");
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+// console.log('Balance:', myAccount.balance);
+
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
